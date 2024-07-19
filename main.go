7f90b51078915fe7d9a5ac07e795aac1b6761a74
Christophe Ayre <christopher.develop@hotmail.com>
2024-07-19 23:57:34 +0800

    gofmt.

--- main.go
+++ main.go
@@ -2,87 +2,86 @@ package main
 
 import (
 	"errors"
+	"flag"
 	"fmt"
-    "os"
+	"os"
 	"time"
-    "flag"
 )
 
 type Config struct {
-    date string
-    time string
-    message string
-    isTodoItem bool
+	date       string
+	time       string
+	message    string
+	isTodoItem bool
 }
 
-func setup() (Config, error){
-    todoPtr := flag.Bool("t", false, "Add a todo item instead of a journal entry")
-    flag.Parse()
-    os.Args = flag.Args()
+func setup() (Config, error) {
+	todoPtr := flag.Bool("t", false, "Add a todo item instead of a journal entry")
+	flag.Parse()
+	os.Args = flag.Args()
 
-    // Looks like golang flags doesn't allow flags after other args so for now we will use this.
-    if len(os.Args) < 1 {
-        return Config{}, errors.New("No arguments provided")
-    }
-    if len(os.Args) > 2 {
-        return Config{}, errors.New("Too many arguments provided, is your message in quotes?")
-    }
+	// Looks like golang flags doesn't allow flags after other args so for now we will use this.
+	if len(os.Args) < 1 {
+		return Config{}, errors.New("No arguments provided")
+	}
+	if len(os.Args) > 2 {
+		return Config{}, errors.New("Too many arguments provided, is your message in quotes?")
+	}
 
-    date := string(os.Args[0])
-    time := time.Now().Format("15:04")
-    message := ""
+	date := string(os.Args[0])
+	time := time.Now().Format("15:04")
+	message := ""
 
-    if len(os.Args) == 1 {
-        // If there is no command line message, open vim for user to input message.
-        data, err := getInputFromVim()
-        message = data
-        if err != nil {
-            return Config{}, err
-        }
-    }else{
-        message = string(os.Args[1])
-    }
+	if len(os.Args) == 1 {
+		// If there is no command line message, open vim for user to input message.
+		data, err := getInputFromVim()
+		message = data
+		if err != nil {
+			return Config{}, err
+		}
+	} else {
+		message = string(os.Args[1])
+	}
 
-    return Config{date: date, time: time, message: message, isTodoItem: *todoPtr}, nil
+	return Config{date: date, time: time, message: message, isTodoItem: *todoPtr}, nil
 }
-
 
 func main() {
-    config, err := setup()
-    if err != nil {
-        fmt.Println(err)
-        os.Exit(1)
-    }
-    convertedDateInput, err := getDateFromInput(config.date)
-    if err != nil {
-        fmt.Println(err)
-        os.Exit(1)
-    }
-    if config.message == "" {
-        fmt.Println("No message provided, exiting.")
-        os.Exit(1)
-    }
+	config, err := setup()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	convertedDateInput, err := getDateFromInput(config.date)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	if config.message == "" {
+		fmt.Println("No message provided, exiting.")
+		os.Exit(1)
+	}
 
-    filename := convertedDateInput + ".md"
-    if err := CreateFileIfNotExists(filename); err != nil {
-        fmt.Println(err)
-        os.Exit(1)
-    }
+	filename := convertedDateInput + ".md"
+	if err := CreateFileIfNotExists(filename); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 
-    //Todos go to the bottom of the file, the rest is placed newest at the top.
-    if config.isTodoItem {
-        message := "- [ ] " + config.message
-        err := appendToFile(filename, message)
-        if err != nil {
-            fmt.Println(err)
-            os.Exit(1)
-        }
-    }else{
-        message := config.time + " " + config.message
-        err := prependToFile(filename, message)
-        if err != nil {
-            fmt.Println(err)
-            os.Exit(1)
-        }
-    }
+	//Todos go to the bottom of the file, the rest is placed newest at the top.
+	if config.isTodoItem {
+		message := "- [ ] " + config.message
+		err := appendToFile(filename, message)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	} else {
+		message := config.time + " " + config.message
+		err := prependToFile(filename, message)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	}
 }
--- userInput.go
+++ userInput.go
@@ -1,52 +1,52 @@
 package main
 
 import (
-    "time"
-    "os"
-    "os/exec"
-    "errors"
+	"errors"
+	"os"
+	"os/exec"
+	"time"
 )
 
 // getDateFromInput takes a string and returns a string in the format "YYYY-MM-DD" or an error.
 func getDateFromInput(dateString string) (string, error) {
-    if dateString == "today" {
-        return time.Now().Format("2006-01-02"), nil
-    }else if dateString == "tomorrow" {
-        return time.Now().AddDate(0, 0, 1).Format("2006-01-02"), nil
-    }else if dateString == "yesterday" {
-        return time.Now().AddDate(0, 0, -1).Format("2006-01-02"), nil
-    }
+	if dateString == "today" {
+		return time.Now().Format("2006-01-02"), nil
+	} else if dateString == "tomorrow" {
+		return time.Now().AddDate(0, 0, 1).Format("2006-01-02"), nil
+	} else if dateString == "yesterday" {
+		return time.Now().AddDate(0, 0, -1).Format("2006-01-02"), nil
+	}
 
-    // Check if the date is in the correct format
-    _, err := time.Parse("2006-01-02", dateString)
-    if err != nil {
-        return "", errors.New("Date is not in the correct format")
-    }
+	// Check if the date is in the correct format
+	_, err := time.Parse("2006-01-02", dateString)
+	if err != nil {
+		return "", errors.New("Date is not in the correct format")
+	}
 
-    // If the date is not a special case and it is in the correct format, use it as is.
-    return dateString, nil
+	// If the date is not a special case and it is in the correct format, use it as is.
+	return dateString, nil
 }
 
 // getInputFromVim opens vim for the user to input a message and returns the message as a string or an error.
 func getInputFromVim() (string, error) {
-    tempFile, err := os.CreateTemp(".", "")
-    defer os.Remove(tempFile.Name())
-    if err != nil {
-        return "", err
-    }
+	tempFile, err := os.CreateTemp(".", "")
+	defer os.Remove(tempFile.Name())
+	if err != nil {
+		return "", err
+	}
 
-    vimCommand := exec.Command("nvim", tempFile.Name())
-    vimCommand.Stdin = os.Stdin
-    vimCommand.Stdout = os.Stdout
-    err = vimCommand.Run()
-    if err != nil {
-        return "", errors.New("Error running nvim")
-    }
+	vimCommand := exec.Command("nvim", tempFile.Name())
+	vimCommand.Stdin = os.Stdin
+	vimCommand.Stdout = os.Stdout
+	err = vimCommand.Run()
+	if err != nil {
+		return "", errors.New("Error running nvim")
+	}
 
-    data, err := os.ReadFile(tempFile.Name())
-    if err != nil {
-        return "", errors.New("Error reading nvim's output")
-    }
+	data, err := os.ReadFile(tempFile.Name())
+	if err != nil {
+		return "", errors.New("Error reading nvim's output")
+	}
 
-    return string(data), nil
+	return string(data), nil
 }
